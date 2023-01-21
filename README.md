1fb8003288022ebfce4d76a9761124534b5d88f9
abdullahalbuianain <AA4644@live.mdx.ac.uk>
2023-01-21 13:42:08 +0300

    frontend commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Github Repository: https://github.com/abdullahalbuianain/abdulla--CW1
+
+Github Pages: https://abdullahalbuianain.github.io/abdulla--CW1/
---
+++ lessons.js
@@ -0,0 +1,98 @@
+let lesson = [
+            {
+                id: 1001,
+                subject: "Math",
+                location: "London",
+                price: 100.00,
+                image: "assets/math.jpg",
+                spaces:5,
+                
+            },
+            {
+                id: 1002,
+                subject: "Math",
+                location: "Oxford",
+                price: 100.00,
+                image: "assets/math.jpg",
+                spaces: 5,
+                
+            },
+            {
+                id: 1003,
+                subject: "English",
+                location: "London",
+                price: 80.00,
+                image: "assets/english.jpg",
+                spaces: 5,
+                
+            },
+            {
+                id: 1004,
+                subject: "English",
+                location: "York",
+                price: 80.00,
+                image: "assets/english.jpg",
+                spaces: 5,
+                
+            },
+            {
+                id: 1005,
+                subject: "Music",
+                location: "Bristol",
+                price: 90.00,
+                image: "assets/musical-notes.jpg",
+                spaces: 5,
+                
+            },
+            {
+                id: 1006,
+                subject: "Music",
+                location: "Leeds",
+                price: 90.00,
+                image: "assets/musical-notes.jpg",
+                spaces: 5,
+               
+            },
+            {
+                id: 1007,
+                subject: "Science",
+                location: "London",
+                price: 100.00,
+                image: "assets/Science.jpg",
+                spaces: 5,
+                
+            },
+            {
+                id: 1008,
+                subject: "Science",
+                location: "York",
+                price: 100.00,
+                image: "assets/Science.jpg",
+                spaces: 5,
+                
+            },
+            {
+                id: 1009,
+                subject: "Social Studies",
+                location: "Leeds",
+                price: 70.00,
+                image: "assets/Social-Studies.jpg",
+                spaces: 5,
+                
+            },
+            {
+                id: 1010,
+                subject: "Social Studies",
+                location: "Bristol",
+                price: 70.00,
+                image: "assets/Social-Studies.jpg",
+                spaces: 5,
+                
+            },
+            
+            ]
+    
+
+
+
+
